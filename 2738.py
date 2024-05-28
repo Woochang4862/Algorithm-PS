aefd9cc9d5884b2525d0fad38a317edc246f886a
woochang4862 <woochang4862@gmail.com>
2024-05-28 15:47:34 +0900

    add 2738.py

---
+++ 2738.py
@@ -0,0 +1,11 @@
+import sys
+
+N, M = tuple(map(int,sys.stdin.readline().split()))
+matrix1 = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+matrix2 = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        print(matrix1[i][j]+matrix2[i][j], end=' ')
+    print()
+
