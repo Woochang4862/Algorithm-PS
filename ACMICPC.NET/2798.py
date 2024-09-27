de8c047329d6718b2aca5d8024d67a68cae3232e
woochang4862 <woochang4862@gmail.com>
2024-09-27 13:49:08 +0900

    2주차 스터디 완료

--- ACMICPC.NET/2798.py
+++ ACMICPC.NET/2798.py
@@ -1,9 +1,9 @@
-from itertools import permutations
+from itertools import combinations # 가능한 모든 조합 계산
 
-N, M = map(int,input().split())
-cards = list(map(int,input().strip().split()))
+N, M = map(int,input().split()) # N : 카드의 개수, M : 목표 숫자
+cards = list(map(int,input().split()))
 res = []
-for p in permutations(cards, 3):
+for p in combinations(cards, 3):
     tmp = sum(p)
     if tmp <= M:
         res.append(tmp)
